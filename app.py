ab10cff317812074774db27de10ee2340defd729
Cameron Berg <54913067+camberg23@users.noreply.github.com>
2024-03-14 11:55:57 -0600

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ scale_options = [f"{row['Scale Name']} ({row['Cat']})" for _, row in sorted_df.d
 selected_scales = st.selectbox("Select which scale you'd like to generate activities for:", scale_options, index=None, key='activities')
 
 if selected_scales:
-    selected_scale_names = [s.split(" (")[0] for s in selected_scales if s]
+    selected_scale_names = [s.split(" (")[0] for s in selected_scales]
     scale_items_dict = qs[qs['Scale Name'].isin(selected_scale_names)].groupby('Scale Name')['Item Text'].apply(list).to_dict()
 
 # prompt = generate_four_activities

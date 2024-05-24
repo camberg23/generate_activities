85712aad905a644e172a61aa71330b69d089530b
Cameron Berg <54913067+camberg23@users.noreply.github.com>
2024-05-24 11:58:09 -0400

    Update system_messages.py

--- system_messages.py
+++ system_messages.py
@@ -364,6 +364,7 @@ YOUR OUTPUTS:
 trait_text_generation = """
 Your job is to generate about two paragraphs of content for someone who scores {LEVEL} in {TRAIT}. Context about how we are defining {TRAIT}: {DESCRIPTION} 
 We present this trait to the user as {UI} and refer to being low in it as {LO} and high in it as {HI}. Please use this language if you are referring to the trait explicitly in your writing, but integrate it naturally (don't capitalize it or treat it as a piece of nomenclature, just integrate these descriptors seamlessly into the writing).
+Do not invoke the name {TRAIT}, instead opt for {UI}, {LO}, {HI}. Again, use these descriptors in a natural way (don't say things like "what we might call 'Challenging'", just say something more like 'people who might be considered more challenging').
 
 The content should be as follows:
 1. One short paragraph related to the STRENGTHS/PROS related to being {LEVEL} in {TRAIT}.
